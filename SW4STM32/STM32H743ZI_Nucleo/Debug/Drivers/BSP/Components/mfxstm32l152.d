2d4918d3d631731a509739bfd150a6255f0576cb
Sergey <sergey@v4scale.com>
2021-10-10 15:42:29 +0300

    feat: init

---
+++ SW4STM32/STM32H743ZI_Nucleo/Debug/Drivers/BSP/Components/mfxstm32l152.d
@@ -0,0 +1,8 @@
+Drivers/BSP/Components/mfxstm32l152.o: \
+ /Users/grishy/Desktop/LwIP_HTTP_Server_Netconn_RTOS/Drivers/BSP/Components/mfxstm32l152/mfxstm32l152.c \
+ /Users/grishy/Desktop/LwIP_HTTP_Server_Netconn_RTOS/Drivers/BSP/Components/mfxstm32l152/mfxstm32l152.h \
+ /Users/grishy/Desktop/LwIP_HTTP_Server_Netconn_RTOS/Drivers/BSP/Components/mfxstm32l152/mfxstm32l152_reg.h
+
+/Users/grishy/Desktop/LwIP_HTTP_Server_Netconn_RTOS/Drivers/BSP/Components/mfxstm32l152/mfxstm32l152.h:
+
+/Users/grishy/Desktop/LwIP_HTTP_Server_Netconn_RTOS/Drivers/BSP/Components/mfxstm32l152/mfxstm32l152_reg.h:
---
+++ SW4STM32/STM32H743ZI_Nucleo/Debug/Drivers/BSP/Components/stmpe811.d
@@ -0,0 +1,16 @@
+Drivers/BSP/Components/stmpe811.o: \
+ /Users/grishy/Desktop/LwIP_HTTP_Server_Netconn_RTOS/Drivers/BSP/Components/stmpe811/stmpe811.c \
+ /Users/grishy/Desktop/LwIP_HTTP_Server_Netconn_RTOS/Drivers/BSP/Components/stmpe811/stmpe811.h \
+ /Users/grishy/Desktop/LwIP_HTTP_Server_Netconn_RTOS/Drivers/BSP/Components/stmpe811/stmpe811_reg.h \
+ ../../../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../../../Drivers/CMSIS/Include/cmsis_gcc.h ../../../Inc/stmpe811_conf.h
+
+/Users/grishy/Desktop/LwIP_HTTP_Server_Netconn_RTOS/Drivers/BSP/Components/stmpe811/stmpe811.h:
+
+/Users/grishy/Desktop/LwIP_HTTP_Server_Netconn_RTOS/Drivers/BSP/Components/stmpe811/stmpe811_reg.h:
+
+../../../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../../../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../../../Inc/stmpe811_conf.h:
